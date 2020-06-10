ad48a76a9498ff8119b95c3574848e2a43405211
Pedro Lopes <pedrolopeshls99@gmail.com>
2020-06-09 22:38:38 -0300

    creating event and list events

--- app/src/main/java/br/com/wep/app/model/Entities/Event.java
+++ app/src/main/java/br/com/wep/app/model/Entities/Event.java
@@ -18,7 +18,7 @@ public class Event {
     @Column(name = "description", nullable = false, length = 600)
     private String description;
 
-    @Column(name = "adress", nullable = false, length = 100)
+    @Column(name = "adress", nullable = true, length = 100)
     private String adress;
 
     @Column(name = "img", nullable = false, length = 200)
@@ -27,11 +27,17 @@ public class Event {
     @Column(name = "privated", nullable = false)
     private boolean privated;
 
-    @Column(name = "createdAt", nullable = false, length = 55)
+//    @Column(name = "createdAt", nullable = false)
     private Date createdAt;
 
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new Date();
+    }
+
+
     @Column(name = "eventeDate", nullable = false)
-    private Date eventeDate;
+    private String eventeDate;
 
     @OneToMany(mappedBy = "event")
     private List<Comment> comments;
@@ -44,7 +50,7 @@ public class Event {
     public Event() {
     }
 
-    public Event(String title, String description, String adress, String img, boolean privated, Date eventeDate, User user) {
+    public Event(String title, String description, String adress, String img, boolean privated, String eventeDate, User user) {
         this.title = title;
         this.description = description;
         this.adress = adress;
@@ -52,5 +58,101 @@ public class Event {
         this.privated = privated;
         this.eventeDate = eventeDate;
         this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public boolean isPrivated() {
+        return privated;
+    }
+
+    public void setPrivated(boolean privated) {
+        this.privated = privated;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getEventeDate() {
+        return eventeDate;
+    }
+
+    public void setEventeDate(String eventeDate) {
+        this.eventeDate = eventeDate;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<User> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<User> likes) {
+        this.likes = likes;
+    }
+
+    public int getConfimed() {
+        return confimed;
+    }
+
+    public void setConfimed(int confimed) {
+        this.confimed = confimed;
     }
 }
