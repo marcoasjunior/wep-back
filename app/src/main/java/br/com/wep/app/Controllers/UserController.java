f5a1c8a8abb12f7bdbb4539af74f3ef25473b9cd
Liam Cabra Teixeira <liam_texeira@estudante.sc.senai.br>
2020-07-08 12:00:16 -0300

    EndPoint /auth

--- app/src/main/java/br/com/wep/app/Controllers/UserController.java
+++ app/src/main/java/br/com/wep/app/Controllers/UserController.java
@@ -2,6 +2,7 @@ package br.com.wep.app.Controllers;
 
 import br.com.wep.app.model.Entities.User;
 import br.com.wep.app.model.Repos.UserRepo;
+import br.com.wep.app.util.md5Password;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +23,30 @@ public class UserController {
         return users;
     }
 
+    //Recebe um JSON com email e password
+    //Se o usuario não existir retorna uma Exception
+    //Caso somente a senha esteja incorreta retorna null
+    //Se email e senha estejam corretos retorna o id do usuario
+    @PostMapping("/auth")
+    public Integer auth(@RequestBody User user) throws Exception {
+        User foundUser = repo.getUserByEmail(user.getEmail());
+
+        if(foundUser == null){
+            throw new Exception("O usuario não existe");
+        };
+
+        if(foundUser.getPassword().equals(md5Password.md5(user.getPassword()))){
+            return foundUser.getId();
+        };
+
+        return null;
+    }
+
     @PostMapping
     public User registerUser(@RequestBody User user){
         try{
+            String password = user.getPassword();
+            user.setPassword(md5Password.md5(password));
             return repo.save(user);
         }catch (Exception e) {
             System.out.println(e);
