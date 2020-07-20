3d6524b41beebcadf024e06c4e2083caba984a70
Marco <marcoasjunior90@gmail.com>
2020-07-20 20:48:37 -0300

    authToken

--- app/src/main/java/br/com/wep/app/Controllers/UserController.java
+++ app/src/main/java/br/com/wep/app/Controllers/UserController.java
@@ -50,6 +50,16 @@ public class UserController {
         }
     }
 
+    @CrossOrigin
+    @PostMapping("/authToken")
+    public Object authToken(@RequestBody String Authentication) throws Exception {
+        try {
+            return TokenService.decodeToken(Authentication);
+        } catch (Exception e) {
+            return e;
+        }
+    }
+
     @PostMapping
     public User registerUser(@RequestBody User user){
         try{
@@ -59,8 +69,9 @@ public class UserController {
 
         }catch (Exception e) {
             System.out.println(e);
+            return null;
         }
-        return null;
+
     }
 
     @GetMapping(path = "/{userID}")
